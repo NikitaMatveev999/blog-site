0982df151e70c3e275aa4187a60c55a345c3c31a
NikitaMatveev999 <NikitaMatveev999@outlook.com>
2023-01-24 23:22:04 +0300

    Initial commit

---
+++ blog/blog/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('posts.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
+    path('users/', include('users.urls')),
+
+]
