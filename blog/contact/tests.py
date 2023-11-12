8670ec77e749bf22a855872ca97fc278414d7400
NikitaMatveev999 <NikitaMatveev999@outlook.com>
2023-11-12 18:00:53 +0300

    add tests for contact

--- blog/contact/tests.py
+++ blog/contact/tests.py
@@ -14,16 +14,10 @@ class ContactCreateViewTest(TestCase):
             'text': 'This is a test message',
         }
 
-    # def test_contact_form_valid(self):
-    #     response = self.client.post(reverse('contact_form'), data=self.contact_data)
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertTrue(Contact.objects.filter(name='Test User').exists())
-
-    @patch('contact.tasks.send_email')
-    def test_contact_form_valid_sends_message(self, mock_send_email):
+    def test_contact_form_valid(self):
         response = self.client.post(reverse('contact_form'), data=self.contact_data)
         self.assertEqual(response.status_code, 302)
-        mock_send_email.assert_called_once_with(self.contact_data['user_email'], self.contact_data['text'])
+        self.assertTrue(Contact.objects.filter(name='Test User').exists())
 
     def test_contact_form_invalid(self):
         invalid_data = self.contact_data.copy()
