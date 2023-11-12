020615d18278eff0f2522aed950f8bebfa2add2b
NikitaMatveev999 <NikitaMatveev999@outlook.com>
2023-11-12 14:57:05 +0300

    add urls for password reset

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path, include, re_path
 from .views import RegistrationView
 
@@ -6,5 +7,17 @@ urlpatterns = [
     path('users/', RegistrationView.as_view(), name='registration'),
     path('register', RegistrationView.as_view(), name='register'),
     re_path('', include('social_django.urls', namespace='social')),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
 
 ]
