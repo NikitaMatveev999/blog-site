cb67f8b9e2308c03abb1f3fa6f81f2bf1bac682c
NikitaMatveev999 <NikitaMatveev999@outlook.com>
2023-11-12 17:34:32 +0300

    add tests for contact

--- blog/contact/tests.py
+++ blog/contact/tests.py
@@ -1,29 +1,30 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from .models import Contact
+from .forms import ContactForm
 
 
 class ContactCreateViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.url = reverse('contact_form')
+        self.contact_data = {
+            'name': 'Test User',
+            'user_email': 'test@example.com',
+            'text': 'This is a test message',
+        }
 
-    def test_contact_create_view(self):
-        response = self.client.get(self.url)
+    # def test_contact_form_valid(self):
+    #     response = self.client.post(reverse('contact_form'), data=self.contact_data)
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertTrue(Contact.objects.filter(name='Test User').exists())
+
+    def test_contact_form_invalid(self):
+        invalid_data = self.contact_data.copy()
+        invalid_data['user_email'] = 'invalid_email'
+        form = ContactForm(data=invalid_data)
+        self.assertFalse(form.is_valid())
+
+    def test_contact_form_view(self):
+        response = self.client.get(reverse('contact_form'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'contact/contact.html')
-
-    def test_contact_create_post(self):
-        data = {
-            'name': 'Test User',
-            'email': 'test@example.com',
-            'message': 'This is a test message',
-        }
-        response = self.client.post(self.url, data)
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/')
-
-        contact = Contact.objects.first()
-        self.assertEqual(contact.name, 'Test User')
-        self.assertEqual(contact.email, 'test@example.com')
-        self.assertEqual(contact.message, 'This is a test message')
